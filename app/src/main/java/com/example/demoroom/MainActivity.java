d421341d47b0c010b335500a8431746630abac04
Federico Trani <federicotrani@gmail.com>
2019-05-21 14:44:33 -0300

    Se agragaron opciones para agregar y eliminar posts

--- app/src/main/java/com/example/demoroom/MainActivity.java
+++ app/src/main/java/com/example/demoroom/MainActivity.java
@@ -7,8 +7,17 @@ import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.example.demoroom.db.Post;
+
+import java.time.LocalDateTime;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class MainActivity extends AppCompatActivity implements PostAdapter.OnDeleteButtonClickListener {
 
@@ -19,6 +28,8 @@ public class MainActivity extends AppCompatActivity implements PostAdapter.OnDel
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.setTitle("Gestor de Posteos");
 
         postAdapter = new PostAdapter(this, this);
 
@@ -34,7 +45,30 @@ public class MainActivity extends AppCompatActivity implements PostAdapter.OnDel
     }
 
     @Override
-    public void onDeleteButtonClicked(Post post) {
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_option_main, menu);
+        return super.onCreateOptionsMenu(menu);
 
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId()==R.id.action_add_post){
+            Calendar calendar = new GregorianCalendar();
+
+            postViewModel.savePost(new Post("Titulo del Post " + calendar.getTime() ,"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."));
+            Toast.makeText(this, "Registro agregado ok", Toast.LENGTH_SHORT).show();
+            return true;
+        }else {
+            return super.onOptionsItemSelected(item);
+        }
+
+    }
+
+    @Override
+    public void onDeleteButtonClicked(Post post) {
+        postViewModel.deletePost(post);
+        Toast.makeText(this, "Registro eliminado ok", Toast.LENGTH_SHORT).show();
     }
 }
